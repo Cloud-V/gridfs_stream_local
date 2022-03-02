e5ed099399600583b8dec41667ee17059ee02235
omardiaa <omardiaa500@gmail.com>
2022-03-02 06:56:52 +0200

    Testing commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Grid = require("./Grid");
 const myGrid = new Grid();
 
 //Testing GridWriteStream
-const writeStream = myGrid.createWriteStream({filename: "newFile"});
+const writeStream = myGrid.createWriteStream({ filename: "newFile" });
 
 writeStream.on("close", file => {
   console.log(`File id: ${file._id}`);
@@ -16,13 +16,13 @@ writeStream.run(); //This will need to be added in the main program
 
 //Testing GridReadStream
 let fsId = "6210a2cfbe97910a16619e32";
-const readStream = myGrid.createReadStream({_id:fsId});
+const readStream = myGrid.createReadStream({ _id: fsId });
 
-readStream.on("data", chunck=>{
+readStream.on("data", chunck => {
   console.log(`Chunk: ${chunck}`)
 })
 
-readStream.on("error", error=>{
+readStream.on("error", error => {
   console.log("Error: ", error);
 })
 
